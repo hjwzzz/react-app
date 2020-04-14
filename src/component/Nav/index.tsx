4958e87a9d0e426b0de047b46a17d43dfc358eb0
hjwzzz <hjwzzz@hotmail.com>
2020-04-14 18:34:09 +0800

    feat: 更新

--- src/component/Nav/index.tsx
+++ src/component/Nav/index.tsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
 import Icon from "./../../component/icon";
+import "./index.less";
+import { CSSTransition } from "react-transition-group";
+
 const Nav = () => {
+  const [searchPageStatus, SetSearchPageStatus] = useState(false);
   return (
-    <div className="nav">
-      <div className="nav-left">
-        <Icon icon="icon-wode"></Icon>
+    <Fragment>
+      <div className="nav">
+        <div className="nav-left">
+          <Icon icon="icon-microphone-alt"></Icon>
+        </div>
+        <div
+          className="nav-content"
+          onClick={() => SetSearchPageStatus(true)}
+        ></div>
+        <div className="nav-right" onClick={() => SetSearchPageStatus(false)}>
+          <div>播放</div>
+        </div>
       </div>
-      <div className="nav-content"></div>
-      <div className="nav-right"></div>
-    </div>
+      <CSSTransition
+        in={searchPageStatus}
+        unmountOnExit
+        timeout={1000}
+        classNames="alert"
+      >
+        <div className="search-page"></div>
+      </CSSTransition>
+    </Fragment>
   );
 };
 
